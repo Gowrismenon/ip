d2647027ae5e11825e4ace3773242deb696bca00
Gowri Menon <thakku.gowri@gmail.com>
2026-01-30 15:06:41 +0800

    Level 1

--- src/main/java/Mal.java
+++ src/main/java/Mal.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Mal {
     public static void main(String[] args) {
-        String logo = "Mal";
-        System.out.println("Hello I'm " + logo +
-                            "\nYou summoned me?\n"+
-                            "_____________________________________________" +
-                            "\nI'm out, Stay rotten to the core!\n" +
-                            "_____________________________________________");
+        Scanner sc = new Scanner(System.in);
 
+        String logo = "Mal";
+        String line = "_______________________________________________";
+        String byeMsg = "Right, I'm out then, Stay rotten to the core!";
+        System.out.println(line +
+                            "\nHello I'm " + logo +
+                            "\nYou summoned me?\n" +
+                            line);
+        while(true) {
+            String input = sc.nextLine();
+            if(input.equals("bye")) {
+                System.out.println(line + "\n" + byeMsg + "\n" + line);
+                break;
+            }
+            System.out.println(line + "\n" + input + "\n" + line);
+        }
+        sc.close();
     }
 }
