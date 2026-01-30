b90cbd3b549116580681be0c3747f095efe91a2c
Gowri Menon <thakku.gowri@gmail.com>
2026-01-30 14:53:32 +0800

    Level 0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mal User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Mal.java
@@ -0,0 +1,11 @@
+public class Mal {
+    public static void main(String[] args) {
+        String logo = "Mal";
+        System.out.println("Hello I'm " + logo +
+                            "\nYou summoned me?\n"+
+                            "_____________________________________________" +
+                            "\nI'm out, Stay rotten to the core!\n" +
+                            "_____________________________________________");
+
+    }
+}
