2587b47c3597be9317383cecb2302469def07b79
Gowri Menon <thakku.gowri@gmail.com>
2026-01-30 15:31:52 +0800

    Level 2

--- src/main/java/Mal.java
+++ src/main/java/Mal.java
@@ -1,23 +1,40 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Mal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        ArrayList<String> list = new ArrayList<>();
+
         String logo = "Mal";
         String line = "_______________________________________________";
-        String byeMsg = "Right, I'm out then, Stay rotten to the core!";
+        String byeMsg = "Finally. I thought you'd never stop talking. Stay rotten.";
         System.out.println(line +
                             "\nHello I'm " + logo +
                             "\nYou summoned me?\n" +
                             line);
         while(true) {
             String input = sc.nextLine();
-            if(input.equals("bye")) {
+            if (input.equals("bye")) {
                 System.out.println(line + "\n" + byeMsg + "\n" + line);
                 break;
             }
-            System.out.println(line + "\n" + input + "\n" + line);
+            if (input.equals("list")) {
+                if (!list.isEmpty()) {
+                    System.out.println(line + "\nHere's the master plan, i guess");
+                    for (int i = 0; i < list.size(); i++) {
+                        int index = i + 1;
+                        System.out.println(index + ". " + list.get(i) + "\n");
+                    }
+                } else {
+                    System.out.println("ummm... there is no chaos yet");
+                }
+                System.out.println(line);
+            } else {
+                list.add(input);
+                System.out.println(line + "\nadded: " + input + "\n" + line);
+            }
         }
         sc.close();
     }
